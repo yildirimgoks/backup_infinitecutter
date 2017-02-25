e609a2bdef93e08e50beff261d956e283c332285
yildirimgoks <goksuyildirim@gmail.com>
2017-02-25 14:45:13 +0300

    New TileSpawner Initiated

--- InfiniteCutter/Assets/Scripts/Spawners/TileSpawner.cs
+++ InfiniteCutter/Assets/Scripts/Spawners/TileSpawner.cs
@@ -10,17 +10,20 @@ public class TileSpawner : MonoBehaviour {
     public float spawnMin = 1;
     public float spawnMax = 2f;
 	private float _tileLength;
-    public GameObject Player;
-    private Vector2 playerPosition;
+    public GameObject Camera;
+    private Vector2 spawnPosition;
+    private Vector2 spawnPlaceDelta;
 
 	// Use this for initialization
 	void Start () {
 		_tileLength = obj [0].transform.lossyScale.x;
+        spawnPlaceDelta = new Vector2(gameObject.transform.position.x - Camera.transform.position.x, gameObject.transform.position.y - Camera.transform.position.y);
         Spawn();
 	}
 	
 	public void Spawn () {
-        Instantiate(obj[Random.Range(0,obj.Length)],transform.position,Quaternion.identity);
+        spawnPosition = new Vector2(Camera.transform.position.x + spawnPlaceDelta.x, Camera.transform.position.y + spawnPlaceDelta.y);
+        Instantiate(obj[Random.Range(0,obj.Length)],spawnPosition,Quaternion.identity);
 		Invoke("Spawn", _tileLength/controller.GetSpeed());
 	}
 
